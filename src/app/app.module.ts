aa1c456ae62242fccd26c1347e5b6e165cf57a1a
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-06-09 15:36:51 -0500

    Ajustar modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,17 +11,55 @@ import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './views/pages/error-page/error-page.component';
 
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs'; // Con el MatTabsModule se hace la invcacion para poder utilizar los tabs de materials
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { MatCardModule } from '@angular/material/card';
+
+// Graficos
+import { NgChartsModule } from 'ng2-charts';
+
+// Tablas de datos
+import { DataTablesModule } from 'angular-datatables';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { WebcamModule } from 'ngx-webcam';
+import { SignaturePadModule } from 'angular2-signaturepad';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ErrorPageComponent,
-  ],
+  declarations: [AppComponent, ErrorPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
+    HttpClientModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatTabsModule,
+    ColorPickerModule,
+    FullCalendarModule,
+    NgbModule,
+    NgChartsModule,
+    CKEditorModule,
+    MatCheckboxModule,
+    MatCardModule,
+    NgApexchartsModule,
+    DataTablesModule,
+    WebcamModule,
+    SignaturePadModule,
   ],
   providers: [
     AuthGuard,
@@ -33,10 +71,15 @@ import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
           xml: () => import('highlight.js/lib/languages/xml'),
           typescript: () => import('highlight.js/lib/languages/typescript'),
           scss: () => import('highlight.js/lib/languages/scss'),
-        }
-      }
-    }
+        },
+      },
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CO',
+    },
+    DatePipe,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
