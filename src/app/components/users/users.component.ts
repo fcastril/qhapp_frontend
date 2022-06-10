cad4768aeb090b8fb775f5123cb22abc5e75ea1d
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-06-10 11:20:36 -0500

    Se organiza la paginación de usuarios

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,77 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { UserModel } from '../../models/user.model';
-import { ApiService } from 'src/app/services/api.service';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "src/app/services/api.service";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import Swal from "sweetalert2";
+import { ViewUserModel } from "src/app/models/v_users.model";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.scss"],
 })
 export class UsersComponent implements OnInit {
+  regs: ViewUserModel[] = [];
+  searchText: "";
+  title = "Usuarios";
+  currentPage: number = 1;
+  records: number = 10;
 
-  regs: UserModel[] = [];
-  searchText: '';
-  title= 'Usuarios';
-
-  constructor(private api: ApiService,
-              private route: Router) { }
+  constructor(private api: ApiService, private route: Router) {}
 
   ngOnInit(): void {
+    this.paginate();
   }
 
-  Submit(Form: NgForm)
-  {
-  }
-
-  search(){
-    this.api.getSearch('users', this.searchText).subscribe(
-      (resp: any)=>{
+  Submit(Form: NgForm) {}
+  paginate() {
+    this.api
+      .getPaginate("Users", this.currentPage, this.records, this.searchText)
+      .subscribe((resp: any) => {
         this.regs = resp;
-      }
-    );
+      });
   }
-  keyupSearch(e: any)
-  {
-    if (e.keyCode === 13)
-    {
+  search() {
+    this.currentPage = 1;
+    this.paginate();
+  }
+  keyupSearch(e: any) {
+    if (e.keyCode === 13) {
       this.search();
     }
   }
-  register(id: number){
-    if (id===0){ // Registro Nuevo
-      this.route.navigate(['user','0']);
+  register(id: number) {
+    if (id === 0) {
+      // Registro Nuevo
+      this.route.navigate(["user", "0"]);
     }
   }
 
-  delete(id: number, idx: number){
-    Swal.fire(
-      {
-        title: 'Eliminar Registro',
-        text: '¿Desea Eliminar el Registro?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Eliminar'
-      }
-    ).then((result)=> {
+  delete(id: number, idx: number) {
+    Swal.fire({
+      title: "Eliminar Registro",
+      text: "¿Desea Eliminar el Registro?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+    }).then((result) => {
       if (result.isConfirmed) {
-        this.api.delete('users', id).subscribe(
-          (resp:any) =>
-          {
-            if (resp.error) {
-                Swal.fire('Error al Eliminar el Registro','Se presentó un error al eliminar el registro', 'error');
-            } else {
-             this.regs.splice(idx,1);
-              Swal.fire('Registro Eliminado', '', 'success');
-
-            }
+        this.api.delete("users", id).subscribe((resp: any) => {
+          if (resp.error) {
+            Swal.fire(
+              "Error al Eliminar el Registro",
+              "Se presentó un error al eliminar el registro",
+              "error"
+            );
+          } else {
+            this.regs.splice(idx, 1);
+            Swal.fire("Registro Eliminado", "", "success");
           }
-        );
-
+        });
       }
     });
   }
-
+  back() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.paginate();
+    }
+  }
+  next() {
+    this.currentPage++;
+    this.paginate();
+  }
 }
---
+++ src/app/models/v_Users.model.ts
@@ -0,0 +1,28 @@
+export class ViewUserModel {
+  idUser: number;
+  idTypeDocument: number;
+  typeDocument: string;
+  documenUser: string;
+  firstNameUser: string;
+  secondNameUser: string;
+  lastFirstNameUser: string;
+  lastSecondNameUser: string;
+  idGender: number;
+  gender: string;
+  dateBirth: Date;
+  emailUser: string;
+  idCity: number;
+  nameCity: string;
+  addressUser: string;
+  cellPhoneUser: string;
+  medicalRegistry: string;
+  attendPatients: boolean;
+  idProfile: number;
+  codeProfile: string;
+  nameProfile: string;
+  pictureUser: string;
+  signature: string;
+  username: string;
+  password: string;
+  active: boolean;
+}
