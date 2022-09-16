217b5b03912b5fc37890c558f8b737727c12aa5f
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-09-16 08:58:04 -0500

    fix bug new/edit profiles - options

--- src/app/components/profiles/profile.component.ts
+++ src/app/components/profiles/profile.component.ts
@@ -1,135 +1,138 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { profile } from 'console';
-import { ApiService } from 'src/app/services/api.service';
-import Swal from 'sweetalert2';
-import { ProfileModel } from '../../models/profile.model';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { profile } from "console";
+import { ApiService } from "src/app/services/api.service";
+import Swal from "sweetalert2";
+import { ProfileModel } from "../../models/profile.model";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styles: [
-  ]
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styles: [],
 })
 export class ProfileComponent implements OnInit {
-
   id: string;
-  title= 'Perfiles';
-  controller= 'profiles';
-  navigateToPage='/profiles';
+  title = "Perfiles";
+  controller = "profiles";
+  navigateToPage = "/profiles";
   subtitle: string;
   reg = new ProfileModel();
   regs: any[] = [];
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private api: ApiService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get("id");
 
-    this.api.getId('ProfileOptions/GetOptions',this.id).subscribe(
-      (resp: any) =>{
+    this.api
+      .getId("ProfileOptions/GetOptions", this.id)
+      .subscribe((resp: any) => {
         this.regs = resp;
-        
+        console.log("resp - profiles", this.regs);
       });
 
-    if (this.id === '0') {
-      this.subtitle = 'CREANDO';
+    if (this.id === "0") {
+      this.subtitle = "CREANDO";
       this.reg = new ProfileModel();
     } else {
-      this.subtitle = 'EDITANDO';
-      this.api.getId(this.controller,this.id).subscribe(
-        (resp: ProfileModel) => this.reg = resp
-      );
-
+      this.subtitle = "EDITANDO";
+      this.api
+        .getId(this.controller, this.id)
+        .subscribe((resp: ProfileModel) => (this.reg = resp));
     }
   }
 
-  Submit( form: NgForm){
+  Submit(form: NgForm) {
     if (form.invalid) {
-      Object.values(form.controls).forEach( ctrl => {
+      Object.values(form.controls).forEach((ctrl) => {
         ctrl.markAsTouched();
       });
 
-      Swal.fire(
-        {
-          title: 'Error',
-          text: 'Hacen falta campos obligatorios',
-          icon: 'error'
-        }
-      );
+      Swal.fire({
+        title: "Error",
+        text: "Hacen falta campos obligatorios",
+        icon: "error",
+      });
       return;
     }
-    Swal.fire(
-      {
-        title: 'Confirmar Guardar !!!',
-        text: '¿Está seguro de guardar el registro actual?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Guardar'
-      }
-    ).then((result)=> {
+    Swal.fire({
+      title: "Confirmar Guardar !!!",
+      text: "¿Está seguro de guardar el registro actual?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Guardar",
+    }).then((result) => {
       if (result.isConfirmed) {
-        if (this.reg.idProfile === 0){
-          this.api.post(this.controller,this.reg).subscribe(
-            (resp: any)=>{
+        if (this.reg.idProfile === 0) {
+          this.api.post(this.controller, this.reg).subscribe((resp: any) => {
             if (resp.error) {
-                Swal.fire('Error al crear el Registro','Se presentó un error al crear el registro', 'error');
+              Swal.fire(
+                "Error al crear el Registro",
+                "Se presentó un error al crear el registro",
+                "error"
+              );
             } else {
               this.router.navigateByUrl(this.navigateToPage);
             }
           });
         } else {
-          this.api.put(this.controller,this.reg, this.reg.idProfile).subscribe(
-            (resp: any)=>{
-            if (resp.error) {
-                Swal.fire('Error al actualizar el Registro','Se presentó un error al actualizar el registro', 'error');
-            } else {
-              this.router.navigateByUrl(this.navigateToPage);
-            }
-          });
+          this.api
+            .put(this.controller, this.reg, this.reg.idProfile)
+            .subscribe((resp: any) => {
+              if (resp.error) {
+                Swal.fire(
+                  "Error al actualizar el Registro",
+                  "Se presentó un error al actualizar el registro",
+                  "error"
+                );
+              } else {
+                this.router.navigateByUrl(this.navigateToPage);
+              }
+            });
         }
       }
     });
   }
 
-  SubmitProfileOptions( form: NgForm){
+  SubmitProfileOptions(form: NgForm) {
     if (form.invalid) {
-      Object.values(form.controls).forEach( ctrl => {
+      Object.values(form.controls).forEach((ctrl) => {
         ctrl.markAsTouched();
       });
 
-      Swal.fire(
-        {
-          title: 'Error',
-          text: 'Hacen falta campos obligatorios',
-          icon: 'error'
-        }
-      );
+      Swal.fire({
+        title: "Error",
+        text: "Hacen falta campos obligatorios",
+        icon: "error",
+      });
       return;
     }
-    Swal.fire(
-      {
-        title: 'Confirmar Guardar Prueba!!!',
-        text: '¿Está seguro de guardar el registro actual?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Guardar'
-      }
-    ).then((result)=> {
+    Swal.fire({
+      title: "Confirmar Guardar Prueba!!!",
+      text: "¿Está seguro de guardar el registro actual?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Guardar",
+    }).then((result) => {
       if (result.isConfirmed) {
-        if (this.regs.length > 0){
-          this.api.post('ProfileOptions',this.regs).subscribe(
-            (resp: any)=>{
+        if (this.regs.length > 0) {
+          this.api.post("ProfileOptions", this.regs).subscribe((resp: any) => {
             if (resp.error) {
-                Swal.fire('Error al crear el Registro','Se presentó un error al crear el registro', 'error');
+              Swal.fire(
+                "Error al crear el Registro",
+                "Se presentó un error al crear el registro",
+                "error"
+              );
             } else {
               this.router.navigateByUrl(this.navigateToPage);
             }
           });
-        } 
+        }
         // else {
         //   this.api.put(this.controller,this.reg, this.reg.idProfile).subscribe(
         //     (resp: any)=>{
@@ -143,5 +146,4 @@ export class ProfileComponent implements OnInit {
       }
     });
   }
-
 }
